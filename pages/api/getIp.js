71656f04da7b02030614f505fdc22b4e8e078631
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-07 22:00:37 -0800

    first commit

--- pages/api/getIp.js
+++
@@ -1,3 +0,0 @@
-export default async (req, res) => {
-    res.status(200).json({ ipaddress:   req.headers['x-forwarded-for'] || req.connection.remoteAddress })
-}
--- pages/api/getOtp/[otpId].js
+++
@@ -1,9 +0,0 @@
-import Redis from 'ioredis'
-
-let redis = new Redis(process.env.REDIS_URL)
-
-export default async (req, res) => {
-    const {otpId} = req.query;
-    const otp = await redis.get(otpId);
-    res.status(200).json({ otp })
-}
